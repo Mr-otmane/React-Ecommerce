3c376edbb661c0e1b933c2c7d8c186afd9ba6338
Mr otmane <otmaneanna2020@gmail.com>
2025-06-02 17:32:01 +0100

    add

---
+++ src/components/Navitems.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navitems = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
+  const [socialToggle, setSocialToggle] = useState(false);
+  const [headerFixed, setHeaderFixed] = useState(false);
+
+  // addevent listener
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      setHeaderFixed(true);
+    } else {
+      setHeaderFixed(false);
+    }
+  });
+  return (
+    <header className={`header-section style-4 ${headerFixed ? "header-fixed fadeInUp" : ""}`}>
+      {/* header top start */}
+      <div className={`header-top d-md-none ${socialToggle ? "open" : ""}`}>
+        <div className="container">
+          <div className="header-top-area">
+            <Link to="/signup" className="lab-btn me-3">
+              <span>Create Account</span>
+            </Link>
+            <Link to="/login">Log in</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navitems;
