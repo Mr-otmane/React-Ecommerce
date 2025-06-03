56be0daf61080fc433c01713e5a5a3c3911e4491
Mr otmane <otmaneanna2020@gmail.com>
2025-06-03 23:37:54 +0100

    add const in this file

--- src/home/Banner.jsx
+++ src/home/Banner.jsx
@@ -23,6 +23,7 @@ const bannerList = [
 ];
 
 const Banner = () => {
+  const [] = useState("")
   const [searchInput, setSearchInput] = useState("");
   const [filteredProducts, setfilteredProducts] = useState("");
   return (
