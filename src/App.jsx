72de1bb571f5cc7eeb2371f48da010ed94baac81
Mr otmane <otmaneanna2020@gmail.com>
2025-06-06 16:47:24 +0100

    create new file "Navitems"in this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navitems from "./components/Navitems";
 
 function App() {
   return (
     <>
-      <nav>Nav bar</nav>
-      <Outlet />
+      <Navitems/>
+      <Outlet/>
     </>
   );
 }
