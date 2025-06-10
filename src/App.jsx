87f3aeb9a2ae637178e500f3b0ec8e5f97f5db1d
Mr otmane <otmaneanna2020@gmail.com>
2025-06-10 18:30:24 +0100

    create new title in this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-     
+      <h1>Hello, Developper</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
