9abdcdda72445d804a23486bf9f6d8a111a21722
Mr otmane <otmaneanna2020@gmail.com>
2025-06-10 21:32:30 +0100

    put Navitems tags in this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navitems from "./components/Navitems";
 
 function App() {
   return (
     <>
-    <nav>Nav bar</nav>
+    <Navitems/>
       <Outlet/>
     </>
   );
