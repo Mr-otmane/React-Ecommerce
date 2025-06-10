010617ee231c24d414ad1dbd01653d1fd8909861
Mr otmane <otmaneanna2020@gmail.com>
2025-06-10 21:07:55 +0100

    add outlet tags in this file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-primary m-5 pt-5">Hello, React Developper</h1>
+      <Outlet/>
     </>
   );
 }
