b5141bfce78d62fa91d0814e55e27ad512c1c92b
Mr otmane <otmaneanna2020@gmail.com>
2025-06-11 23:32:57 +0100

    put span tag in "Link"

--- src/components/Navitems.jsx
+++ src/components/Navitems.jsx
@@ -21,7 +21,7 @@ const Navitems = () => {
       <div>
         <div className="container">
           <div className="header-top-area">
-            <Link to="/signup">Create Account</Link>
+            <Link to="/signup"><span>Create Account</span></Link>
           </div>
         </div>
       </div>
