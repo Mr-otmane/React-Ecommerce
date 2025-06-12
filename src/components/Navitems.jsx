4269ad36d9d296619d50133624e10b6e573388cf
Mr otmane <otmaneanna2020@gmail.com>
2025-06-12 23:31:25 +0100

    put name for navbar link

--- src/components/Navitems.jsx
+++ src/components/Navitems.jsx
@@ -50,14 +50,25 @@ const Navitems = () => {
 
             {/* menu area */}
             <div className="menu-area">
-                <div className="menu">
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/">Home</Link></li>
-                    </ul>
-                </div>
+              <div className="menu">
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/shop">Shop</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog">Blog</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">About</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact">Contact</Link>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
