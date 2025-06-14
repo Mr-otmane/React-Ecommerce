dbc3c1bc77d922669f249b71fae82d4ae2a2f09f
Mr otmane <otmaneanna2020@gmail.com>
2025-06-14 22:16:37 +0100

    put banner tags and his three classname

--- src/Home/banner.jsx
+++ src/Home/banner.jsx
@@ -2,8 +2,30 @@ import React from "react";
 
 const title = <h2>Search Your One From Thousand Of Products</h2>;
 
+const desc = "we have the largest collection of products";
+const bannerList = [
+  {
+    iconName: "icofont-users-alt-4",
+    text: "1.5 Million Customers",
+  },
+  {
+    iconName: "icofont-notification",
+    text: "More then 2000 Marchent",
+  },
+  {
+    iconName: "icofont-globe",
+    text: "Buy Anything Online",
+  },
+];
+
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="banner-section style-4">
+      <div className="container">
+        <div className="banner-content">{title}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
