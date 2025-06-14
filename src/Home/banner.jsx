db952edd510c48bb3f8657502d7109cab5c704d5
Mr otmane <otmaneanna2020@gmail.com>
2025-06-14 22:06:56 +0100

    make new title in this file

--- src/Home/banner.jsx
+++ src/Home/banner.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+
+const title = <h2>Search Your One From Thousand Of Products</h2>;
 
 const Banner = () => {
-  return (
-    <div>Banner</div>
-  )
-}
+  return <div>Banner</div>;
+};
 
-export default Banner
+export default Banner;
