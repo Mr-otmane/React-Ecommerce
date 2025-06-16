bb5c9fedaa8a13d8a80bdbe0e4e22014187a94b4
Mr otmane <otmaneanna2020@gmail.com>
2025-06-16 23:52:48 +0100

    import selectedCategory in this file

--- src/Home/banner.jsx
+++ src/Home/banner.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import productData from "../products.json";
 import { Link } from "react-router-dom";
+import SelectedCategory from "../components/SelectedCategory";
 
 const title = (
   <h2>
@@ -46,6 +47,7 @@ const Banner = () => {
         <div className="banner-content">
           {title}
           <form>
+            <SelectedCategory/>
             <input
               type="text"
               none="search"
