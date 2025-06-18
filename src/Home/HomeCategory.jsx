804cbc88a18b9fdabe7e3e70284ef5c414dbae51
Mr otmane <otmaneanna2020@gmail.com>
2025-06-18 12:05:45 +0100

    add new commint

--- src/Home/HomeCategory.jsx
+++ src/Home/HomeCategory.jsx
@@ -53,11 +53,12 @@ const HomeCategory = () => {
         </div>
         {/* section card */}
         <div className="section-wrapper">
-          <div className="row g-4 justify-center row-cols-md-3 row-cols-sm-2">
+          <div className="row g-4 justify-center row-cols-md-3 row-cols-sm-2 row-cols-1">
             {categoryList.map((val, i) => (
               <div key={i} className="col">
                 <Link to="/shop" className="category-item">
                   <div className="category-inner">
+                    {/* image thumbnail */}
                     <div className="category-thumb">
                       <img src={val.imgUrl} alt="" />
                     </div>
