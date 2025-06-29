ea578fda30ada7f1e96def584667c9833b1e6884
Mr otmane <otmaneanna2020@gmail.com>
2025-06-29 23:36:07 +0100

    add button tags in this file

--- src/Home/banner.jsx
+++ src/Home/banner.jsx
@@ -60,6 +60,7 @@ const Banner = () => {
               <i className="icofont-search"></i>
             </button>
           </form>
+          <button></button>
           <p>nuinur</p>
           <p>{desc}</p>
           <ul className="lab-ul">
